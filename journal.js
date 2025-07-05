8bff7ca837a03aa00e27aad54bcfa0b8df1481b7
Andrew Zemoytin <andrewzemoytin2005@gmail.com>
2025-07-05 10:36:39 -0700

    Add sum() accumlulator

--- journal.js
+++ journal.js
@@ -8,4 +8,17 @@ function isEven(n) {
   return n % 2 === 0;
 }
 
+// Sum an array of numbers
+function sum(arr) {
+  return arr.reduce((acc, x) => acc + x, 0);
+}
 
+// Find the largest number in an array
+function max(arr) {
+  return Math.max(...arr);
+}
+
+// Reverse a string
+function reverse(str) {
+  return str.split('').reverse().join('');
+}
